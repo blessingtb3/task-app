0eea186e434ea9aa27ea9e6fee54765b81f4f137
blessingtb3 <blessingtb3@gmail.com>
2025-02-02 07:30:43 +0200

    adding user component

--- ng-client/src/app/app.component.ts
+++ ng-client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { UserComponent } from './components/user/user.component';
 
 //decorator that tells Angular that this class is a component and adds metadata to it
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
